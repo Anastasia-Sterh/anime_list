0b4702e96f036232dd0ee04721567417869f7588
Anastasia-Sterh <80683348+Anastasia-Sterh@users.noreply.github.com>
2022-12-22 17:26:08 +0300

    first ver

---
+++ dnevnik.js
@@ -0,0 +1,225 @@
+// ОБЪЯВЛЕНИЕ / ДЕКЛАРАЦИЯ (от declare - объявить)
+
+
+let wantToSee = [];
+let alreadySeen = [];
+
+function save() {
+    localStorage.wantToSee = JSON.stringify(wantToSee);
+    localStorage.alreadySeen = JSON.stringify(alreadySeen);
+}
+
+function load() {
+    wantToSee = JSON.parse(localStorage.wantToSee)
+    alreadySeen = JSON.parse(localStorage.alreadySeen)
+
+}
+
+function renderList(parent, animes) {
+    let children = parent.querySelectorAll('.new_el')
+    for (const el of children) {
+        el.remove()
+    }
+
+    let elIndex = 0;
+    for (const anime of animes) {
+        let new_el = document.createElement('div');
+        new_el.classList.add('new_el');
+        new_el.dataset.index = elIndex;
+
+        elIndex = elIndex + 1;
+
+
+        parent.append(new_el)
+
+        let new_el_text = document.createElement('div')
+        let new_el_button_delete = document.createElement('button')
+        let new_el_button_move = document.createElement('button')
+
+        new_el_text.innerText = anime;
+
+
+        new_el_text.style.width = '100%'
+
+        new_el_button_delete.innerText = 'x';
+        new_el_button_delete.className = 'new_el_buttons';
+        new_el_button_delete.onclick = delete_item;
+
+
+        if (parent.classList.contains('list__right')) {
+            new_el_button_move.innerText = '<'
+        } else {
+            new_el_button_move.innerText = '>'
+        }
+
+        new_el_button_move.className = 'new_el_buttons';
+        new_el_button_move.onclick = move_item;
+
+        new_el.append(new_el_text);
+        new_el.append(new_el_button_delete);
+        new_el.append(new_el_button_move);
+    }
+}
+
+function render() {
+    // console.log('Want to see:', wantToSee)
+    // console.log('Already seen:', alreadySeen)
+
+    renderList(document.querySelector('.list__left'), wantToSee);
+    renderList(document.querySelector('.list__right'), alreadySeen);
+}
+
+function click(event) {
+    let open = this; // В this будет элемент, по которому кликнули
+
+    let list_1 = open.querySelector('.list-1')
+    if (open.className == 'open_list') {
+        open.className = 'list'
+        list_1.style.display = 'none';
+    } else {
+        open.className = 'open_list'
+        list_1.style.display = 'block';
+    }
+}
+
+
+function inputEnter(event) {
+    if (event.key == 'Enter') {
+        add.apply(this.nextElementSibling)
+    }
+}
+
+function inputOnFocus() {
+
+    if (this.classList.contains('invalid')) {
+        this.classList.remove('invalid');
+
+    } else {
+        this.setAttribute('class', 'input invalid')
+    }
+}
+
+function add() {
+    let button = this;
+    let parent = button.parentNode;
+    let cnt_input = parent.querySelector('input')
+
+
+
+    if (cnt_input.value == '') {
+        alert('Пустое поле для лохов')
+        return
+    } else if (parent.classList.contains('list__right')) {
+        alreadySeen.push(cnt_input.value);
+        cnt_input.value = '';
+    } else {
+        wantToSee.push(cnt_input.value);
+        cnt_input.value = '';
+    }
+
+
+
+    save()
+    render();
+
+}
+
+function delete_item(event) {
+
+    let target = event.target;
+    let parent = target.parentNode;
+    let parent_1 = parent.parentNode;
+
+    if (parent_1.classList.contains('list__right')) {
+        alreadySeen.splice(parent.dataset.index, 1);
+
+    } else {
+        wantToSee.splice(parent.dataset.index, 1)
+
+    }
+    save()
+    render()
+
+}
+
+
+function delete_all(event) {
+
+    let target = event.target;
+    console.log(target)
+    let parent = target.parentNode;
+    let parent_1 = parent.parentNode;
+    console.log(parent_1, 'родитель путки')
+    let btn_delete_all = document.querySelector('.btn_delete_all');
+    console.log(btn_delete_all.parentNode, 'родитель жмякнутой путки')
+
+
+    if (btn_delete_all.parentNode.classList.contains('list__right')) {
+        alreadySeen.splice(0, alreadySeen.length);
+
+    } else {
+        wantToSee.splice(0, wantToSee.length);  //шо за нах... о_О
+
+    }
+    save()
+    render()
+
+}
+
+function move_item(event) {
+
+    let target = event.target;
+    let parent = target.parentNode;
+    let parent_1 = parent.parentNode;
+
+    if (parent_1.classList.contains('list__right')) {
+        let el = alreadySeen.splice(parent.dataset.index, 1);
+        wantToSee.push(el[0])
+
+    } else {
+        let el = wantToSee.splice(parent.dataset.index, 1)
+        alreadySeen.push(el[0])
+    }
+    save()
+    render()
+
+
+}
+
+function list_1_click(event) {
+    event.stopPropagation();
+}
+
+// ---------------------------------------------------------
+
+// РЕАЛИЗАЦИЯ / ИМПЛЕМЕНТАЦИЯ (от implement - реализовать)
+
+load();
+render();
+
+
+
+for (const el of document.querySelectorAll('.list-1')) {
+    el.addEventListener('click', list_1_click)
+}
+
+for (const el of document.querySelectorAll('.list-1 input')) {
+    el.addEventListener('focus', inputOnFocus);
+    el.addEventListener('keydown', inputEnter);
+}
+
+for (const el of document.querySelectorAll('.list-1 .btn')) {
+    el.addEventListener('click', add);
+}
+
+let divs_arr = document.querySelectorAll('.list')
+for (const el of divs_arr) {
+    el.addEventListener('click', click)
+
+}
+
+let btn_delete_all = document.querySelectorAll('.btn_delete_all');
+for (const el of btn_delete_all) {
+    el.addEventListener('click', delete_all)
+
+}
---
+++ index.js
@@ -0,0 +1,83 @@
+
+let titles;
+const anime_list = async () => {
+    const response_1 = await fetch('https://shikimori.one/api/animes?limit=50&order=popularity&page=1')
+    const titles_1 = await response_1.json()
+    const response_2 = await fetch('https://shikimori.one/api/animes?limit=50&order=popularity&page=2')
+    const titles_2 = await response_2.json()
+    const response_3 = await fetch('https://shikimori.one/api/animes?limit=50&order=popularity&page=3')
+    const titles_3 = await response_3.json()
+
+    const titles = titles_1.concat(titles_2, titles_3);
+    return titles;
+}
+
+
+
+anime_list().then((result) => {
+    titles = result;
+    console.log(titles);
+});
+
+
+
+
+const onButtonClick = function () {
+    let text = document.querySelector('#left_section');
+
+    if (titles.length == 0) {
+        text.innerText = 'Эй, сначала эти посмотри! Уходи отсюда.'
+        return;
+    }
+
+    const rand = Math.floor(Math.random() * titles.length);
+
+    let anime = titles[rand]
+
+    text.innerHTML = `
+        <div id='new_anime'>
+            <img src="https://shikimori.one${anime.image.original}">
+           <div id='new_anime_p'>
+            <p><b>${anime.name} / ${anime.russian}</b></p>
+            <p>Рейтинг: ${anime.score}</p>
+            <p>Эпизоды: ${anime.episodes}</p>
+            <p>Дата выхода: ${anime.aired_on}</p>
+            <p><a target="_blank" href="https://shikimori.one${anime.url}">Подробнее</a></p>
+            </div>
+        </div>
+    `;
+
+    //text.innerText = anime.name;
+    text.style.fontSize = '20px';
+
+    console.log(titles[rand]);
+
+    let div_for_btn = document.createElement('div');
+    text.append(div_for_btn)
+
+    let btn = document.createElement('button');
+    div_for_btn.append(btn);
+    btn.className = 'sliding-button';
+    btn.style.float = 'left';
+    btn.style.marginTop = '25px';
+    btn.innerText = 'Again!';
+    btn.onclick = onButtonClick;
+
+    let btn_2 = document.createElement('button');
+    div_for_btn.append(btn_2);
+    btn_2.className = 'sliding-button';
+    btn_2.style.marginTop = '25px';
+    btn_2.innerText = 'Add to wishlist';
+    //btn_2.onclick = ;
+
+    titles.splice(rand, 1)
+}
+
+document.querySelector('.sliding-button').onclick = onButtonClick;
+
+
+// Создать класс, где будет описание или оценка или картинка, в общем что-то, описывающее аниме.
+//Создать сто тыщ объектов, запихать их в массив и сделать все то же самое
+// сделать ссылку на мой дневник, сверстать там бог весть как что-то
+// можно записывать свои аниме просмотренные с помощью кнопочек и удалять их
+//наверное там надо будет сделать форму авторизации или типа того? Дневник оставим на попозже
